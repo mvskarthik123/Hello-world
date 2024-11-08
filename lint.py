f71a20d0e812ce7759a620b8d61cf084acc44a0a
mvskarthik101 <mvskarthikeya101@gmail.com>
2024-11-08 17:23:40 +0530

    changed1

--- lint.py
+++ lint.py
@@ -5,6 +5,6 @@ THRESHOLD =9
 run=lint.Run(["factorial1.py"],exit=False)
 score=run.linter.stats.global_note
 if score<THRESHOLD:
-    print("Linter failed: score<threshold value")
+    print("Linter failed: score<threshold value to change")
     sys.exit(1)
 sys.exit(0)
